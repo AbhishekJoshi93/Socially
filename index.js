13132af795c4f0a04b47aa9ec7df97dc7f91e074
= <=>
2020-06-28 17:18:12 +0530

    Commit

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const app = express();
 app.use(express.static(__dirname + "/public"));
 app.set("view engine","ejs");
 app.use(bodyParser.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(fileupload());
